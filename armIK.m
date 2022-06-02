484f4ea27df706c0bbacc235a019c33143fe59da
Ahmed Benyoucef <105916158+Ahmed-Benyoucef@users.noreply.github.com>
2022-06-02 11:18:17 +0200

    Update armIK.m

--- armIK.m
+++ armIK.m
@@ -1,6 +1,6 @@
 function [th] = armIK(px,py,pz)
-% Arm robot RRR
-% arm link
+% Arm Robot RRR
+% Arm links
 l1=8;
 l2=10;
 l3=10;
@@ -16,12 +16,12 @@ th3= acos((k^2-l2^2-l3^2)/(2*l2*l3));
 %th2
 beta=atan2(pz-l1,r);
 alpha= atan2(l3*sin(th3),(l2+l3*cos(th3)));
-
 th2=beta-alpha;
+
+% transfer to deg
 th1=rad2deg(th1);
 th2=rad2deg(th2);
 th3=rad2deg(th3);
 
 th=[th1 th2 th3]
 end
-
